7625513c770f354974756ba0490488df7cb1d90a
Olga Nadongo <winnienadongo@gmail.com>
2022-06-15 07:04:39 +0300

    commit tree 78d837537888481a79b792fe3691fc6738155b74

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded" , function(){
+//     console.log("The dom has loaded")
+// } );
+// console.log("This fires when index.js loads- before DOMContentLoaded is triggered")
+
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("JavaScript is so cool. It lets me add text to my page programmatically." )
+    document.querySelector("#text").textContent="This is really cool!"
+
+})
+console.log("This is really cool!")
+
+
