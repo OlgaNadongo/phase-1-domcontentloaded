33c9c9e361457a85f13a72634e51540eb30daef3
Olga Nadongo <winnienadongo@gmail.com>
2022-06-15 07:05:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+document.addEventListener("DOMContentLoaded",function(){
+    console.log("JavaScript is so cool. It lets me add text to my page programmatically." )
+    document.querySelector("#text").textContent="This is really cool!"
+
+})
+console.log("This is really cool!")
+
+
